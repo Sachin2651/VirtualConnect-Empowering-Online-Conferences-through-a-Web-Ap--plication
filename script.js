215ead44af041d211a2343a1a0af2b8f6a2b252d
Sachin2651 <sachinkumarcvru@gmail.com>
2026-04-08 15:10:32 +0530

    Change PeerJS host to 0.peerjs.com
    Updated PeerJS host to a new server address.

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ const peers = {};
 
 // Initialize PeerJS
 const myPeer = new Peer(undefined, {
-  host: "peerjs-server.herokuapp.com",
+  host: "0.peerjs.com",
+  port: 443,
   secure: true,
-  port: 443
+  config: {
+    iceServers: [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun1.l.google.com:19302" },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject"
+      }
+    ]
+  }
 });
 
 let myVideoStream;
